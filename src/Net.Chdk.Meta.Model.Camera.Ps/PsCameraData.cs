f34d8b803e6a4d2142804a21141f7a89a7a67101
Dmitry Shechtman <damnian@gmail.com>
2017-07-08 00:54:23 +0300

    Add PsCardData

--- src/Net.Chdk.Meta.Model.Camera.Ps/PsCameraData.cs
+++ src/Net.Chdk.Meta.Model.Camera.Ps/PsCameraData.cs
@@ -1,6 +1,6 @@
 ﻿namespace Net.Chdk.Meta.Model.Camera.Ps
 {
-    public sealed class PsCameraData : CameraData<PsCameraData, PsCameraModelData>
+    public sealed class PsCameraData : CameraData<PsCameraData, PsCameraModelData, PsCardData>
     {
         public AltData Alt { get; set; }
     }
